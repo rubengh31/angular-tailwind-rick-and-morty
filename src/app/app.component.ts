ab57a8bc840fe02171d7f7b6641da3868c92652c
Ruben Gomez <fend.development@gmail.com>
2023-02-03 09:19:47 +0100

    theme switch color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'application';
+  setMode = false;
+
+  receiveMode(event: any) {
+    this.setMode = event;
+    console.log('MODEEEE', this.setMode);
+  }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
@@ -8,8 +8,17 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  @Output() mode = new EventEmitter<boolean>();
+  setDark = false;
+
   constructor(
     @Inject(DOCUMENT) public document: Document,
     public auth: AuthService
   ) {}
+
+  onChangeToggle() {
+    this.setDark = !this.setDark;
+    this.mode.emit(this.setDark);
+    // console.log(this.setDark);
+  }
 }
